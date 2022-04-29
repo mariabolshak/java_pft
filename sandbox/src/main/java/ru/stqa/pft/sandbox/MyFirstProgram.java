61283ba5d876a3930499f86488e2971164e90df1
mariabolshak <m.bolshakova@t-systems.com>
2022-04-29 07:10:15 +0300

    Первая часть задания "Вычисление расстояния между двумя точками"
    Задание №2: Потренироваться использовать функции, классы, объекты и методы
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
    
    2. Создать функцию
    public static double distance(Point p1, Point p2)
    которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
    
    3. Сделать запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {...}
    и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,22 +4,26 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
     hello("Masha");
+    Square s = new Square(5);
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Rectangle r = new Rectangle(3, 8);
 
-    double a = 3;
-    double b=8;
-    System.out.println("Площадь прямоугольника со сторонами " + a +" и "+b+ " = " + area(a,b));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(4, 2);
+    System.out.println("расстояние между двумя точками координат " + "p1(" + p1.x + "," + p1.y + ")" + " и " + "p2(" + p2.x + "," + p2.y + ")" + " равно " + distance(p1, p2));
+
+
   }
-    public static void hello(String somebody){
-      System.out.println("Hello, " + somebody + "!");
-    }
 
-    public static double area(double len) {
-      return len * len;
-    }
-    public static double area(double a,double b) {
-    return a*b;
-    }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
+
+}
