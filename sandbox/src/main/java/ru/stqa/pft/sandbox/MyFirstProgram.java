8e7cdfcda6ad66833817080a86f71c4f163bbe1f
mariabolshak <m.bolshakova@t-systems.com>
2022-04-29 15:06:04 +0300

    Изменила ответ на 4-е задание "Вычисление расстояния между точками"
    Исправила параметр в функции класса Point и в запускаемом классе исправила обращение к этой функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,7 +13,7 @@ public class MyFirstProgram {
 
     Point p1 = new Point(2, 3);
     Point p2 = new Point(4, 2);
-    System.out.println("расстояние между двумя точками координат " + "p1(" + p1.x + "," + p1.y + ")" + " и " + "p2(" + p2.x + "," + p2.y + ")" + " равно " + p1.distance(p1, p2));
+    System.out.println("расстояние между двумя точками координат " + "p1(" + p1.x + "," + p1.y + ")" + " и " + "p2(" + p2.x + "," + p2.y + ")" + " равно " + p1.distance(p2));
 
 
   }
