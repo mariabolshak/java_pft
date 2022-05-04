66ced8be3d5cd8728f44489ea9e03c56068fd4ac
mariabolshak <m.bolshakova@t-systems.com>
2022-05-04 14:23:39 +0300

    Отформатировала тест про точки координат

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -6,14 +6,15 @@ import org.testng.annotations.Test;
 public class PointTest {
   @Test
   public void testDistance() {
-    Point p1=new Point(2,3);
-    Point p2=new Point(4,6);
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(4, 6);
     Assert.assertEquals(p1.distance(p2), 3.605551275463989);
-      }
+  }
+
   @Test
   public void testDistanceWith0() {
-    Point p1=new Point(3,0);
-    Point p2=new Point(0,0);
+    Point p1 = new Point(3, 0);
+    Point p2 = new Point(0, 0);
     Assert.assertEquals(p1.distance(p2), 3);
   }
 }
