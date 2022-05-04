b16bffefd8e51361b315f1a37e1c175a0836a834
mariabolshak <m.bolshakova@t-systems.com>
2022-05-04 14:21:11 +0300

    Сгруппировала параметры метода заполнения формы в объект, сделать вспомогательный "модельный" класс GroupData для представления таких объектов.
    Делала сначала с помощью refactor introduce Parameter object, что привело к созданию класса, но командой public record...
    Переписала класс и добавила get к параметрам в методе.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -49,13 +49,13 @@ public class GroupCreationTests {
   private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.name());
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.header());
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   private void initGroupCreation() {
