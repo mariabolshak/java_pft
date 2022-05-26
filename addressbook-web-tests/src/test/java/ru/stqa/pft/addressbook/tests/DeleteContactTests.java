797dc31d062205c8cbeefca7cdada36d11a17909
mariabolshak <m.bolshakova@t-systems.com>
2022-05-26 08:29:12 +0300

    Добавила предусловие наличия группы для DeleteContactTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteContactTests.java
@@ -2,10 +2,15 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class DeleteContactTests extends TestBase {
   @Test
   public void testDeleteContact() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getNavigationHelper().goToHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Maria", "Anna", "Bolshakova", "masha", "DT", "TT", "Nevskiy", "56786", "909876", "4564564", "456456", "dd@t.ru", "test1"));
