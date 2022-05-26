ee461624732a41d1d0b8acbf83feb275b34f6b88
mariabolshak <m.bolshakova@t-systems.com>
2022-05-26 23:42:11 +0300

    Формирую коллекциюб сравниваю размеры списков для групп
    Получаем список элементов с информацией о группах, последовательно перебираю их в циклеб извлекая текст, на основе этой инф-ии строим собственные объекты типа GroupData и формировать из этих объектов список, который будет в дальнейшем в тестах использоваться для сравнения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,12 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends BaseHelper {
 
@@ -35,7 +39,7 @@ public class GroupHelper extends BaseHelper {
 
   public void selectGroup(int index) {
     wd.findElements(By.name("selected[]")).get(index).click();
-      }
+  }
 
   public void initGroupModification() {
     click(By.name("edit"));
@@ -58,5 +62,16 @@ public class GroupHelper extends BaseHelper {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
