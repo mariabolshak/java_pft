48a52cbdb07b9e60716a66b048e3c287b87948cc
mariabolshak <m.bolshakova@t-systems.com>
2022-05-29 19:43:59 +0300

    4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования
    Применение лямбда выражения при вычислении максимального значения идентификатора (GroupCreationTest)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,18 +13,13 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    GroupData group=new GroupData("test1", null,null);
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-int max=0;
-for (GroupData g:after)
-  if (g.getId()>max){
-    max=g.getId();
-  }
-group.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
