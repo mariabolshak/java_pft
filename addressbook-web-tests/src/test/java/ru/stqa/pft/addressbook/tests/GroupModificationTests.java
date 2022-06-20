4a6996914801711a5e728b7309b6d4eb9a3ababa
mariabolshak <m.bolshakova@t-systems.com>
2022-06-20 05:44:42 +0300

    Лекция 5.2. Улучшение кода тестов: ещё немного реструктуризации кода. Вынесла предусловия для модификации и удаления групп в аннатацию BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,13 +12,17 @@ import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
-  @Test
-  public void testGroupModification() {
+  @BeforeMethod
+  public void ensurePreconditions(){
     app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
+    if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    List<GroupData> before = app.getGroupHelper().getGroupList();
+  }
+
+  @Test
+  public void testGroupModification() {
+     List<GroupData> before = app.getGroupHelper().getGroupList();
         int index=before.size()-1;
     GroupData group=new GroupData(before.get(index).getId(),"test1","test2","test3");
     app.getGroupHelper().modifyGroup(index, group);
