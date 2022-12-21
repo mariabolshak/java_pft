e4f5e8ac77d91abde9b167f194bdeec232228262
mariabolshak <m.bolshakova@t-systems.com>
2022-12-21 03:42:43 +0300

    Добавлен новый модуль для тестов для Mantis

--- mantis-tests/src/test/java/ru/stqa/pfta/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pfta/mantis/appmanager/ApplicationManager.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook.appmanager;
+package ru.stqa.pfta.mantis.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -20,12 +20,8 @@ public class ApplicationManager {
     private final Properties properties;
 
     public WebDriver wd;
-    private SessionHelper sessionHelper;
-    private NavigationHelper navigationHelper;
-    private GroupHelper groupHelper;
-    private ContactHelper contactHelper;
+
     private final String browser;
-    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         properties = new Properties();
@@ -37,7 +33,7 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        dbHelper = new DbHelper();
+
 
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -48,11 +44,7 @@ public class ApplicationManager {
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
-        groupHelper = new GroupHelper(wd);
-        navigationHelper = new NavigationHelper(wd);
-        sessionHelper = new SessionHelper(wd);
-        contactHelper = new ContactHelper(wd);
-        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+
     }
 
 
@@ -66,22 +58,12 @@ public class ApplicationManager {
     }
 
 
-    public GroupHelper group() {
-        return groupHelper;
-    }
+
 
     public void stop() {
         wd.findElement(By.linkText("Logout")).click();
         wd.quit();
     }
 
-    public NavigationHelper goTo() {
-        return navigationHelper;
-    }
 
-    public ContactHelper contact() {
-        return contactHelper;
-    }
-
-    public DbHelper db() { return dbHelper;}
 }
