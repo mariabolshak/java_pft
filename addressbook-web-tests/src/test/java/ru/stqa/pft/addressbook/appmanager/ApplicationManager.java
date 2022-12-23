549c52eae3b6025ca5c48531dd043e3f9684308e
mariabolshak <m.bolshakova@t-systems.com>
2022-12-23 14:41:42 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -38,13 +41,19 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
+        if ("".equals(properties.getProperty("selenium.server"))) {
 
-        if (Objects.equals(browser, BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (Objects.equals(browser, BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+            if (Objects.equals(browser, BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (Objects.equals(browser, BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (Objects.equals(browser, BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
@@ -83,5 +92,7 @@ public class ApplicationManager {
         return contactHelper;
     }
 
-    public DbHelper db() { return dbHelper;}
+    public DbHelper db() {
+        return dbHelper;
+    }
 }
